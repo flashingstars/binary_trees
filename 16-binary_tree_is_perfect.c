86cf93e1b043f30c5fbce24c49c28e01153d877d
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-03-02 11:04:41 -0800

    If tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -34,7 +34,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-
 	if (left < right)
 		max = right;
 	else
@@ -62,7 +61,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  *
  * @tree: pointer to the root node of the tree to check
  *
- * Return: 0 if perfect, 1 if not
+ * Return: 1 if perfect, 0 if not
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
